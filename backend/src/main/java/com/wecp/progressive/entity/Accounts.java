1ae5db9c7b51d5e52b89fb51346402a7a8b4522c
username <wecp-dummy@gmail.com>
2024-01-20 08:18:04 +0000

    My first commit

--- backend/src/main/java/com/wecp/progressive/entity/Accounts.java
+++ backend/src/main/java/com/wecp/progressive/entity/Accounts.java
@@ -1,7 +1,9 @@
 package com.wecp.progressive.entity;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+
 public class Accounts {
 system.out.println("Hi");
-
+//commit
 System.out.println("hello");
 }
